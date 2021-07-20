323fc65de7b002743b949af40d21605634385907
Mmadi Zaïb <zaib2010@gmail.com>
2021-07-20 16:24:35 +0200

    add finesse swift mailer default plugin

--- src/Model/User/Service/ResetTokenSender.php
+++ src/Model/User/Service/ResetTokenSender.php
@@ -12,19 +12,16 @@ class ResetTokenSender
 {
     private $mailer;
     private $twig;
-    private $from;
 
-    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    public function __construct(\Swift_Mailer $mailer, Environment $twig)
     {
         $this->twig = $twig;
         $this->mailer = $mailer;
-        $this->from = $from;
     }
 
     public function send(Email $email, ResetToken $token)
     {
         $message = (new \Swift_Message('Password resetting'))
-            ->setFrom('mail@app.test', 'APP')
             ->setTo($email->getValue())
             ->setBody($this->twig->render('mail/user/reset.html.twig', [
                 'token' => $token->getToken()
