d98b8e75f2ea6fa38f2c29da665bb2ed122d15aa
Mmadi Zaïb <zaib2010@gmail.com>
2021-07-21 13:08:54 +0200

    Implement Auth login form

---
+++ src/Security/UserIdentity.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserIdentity implements UserInterface
+{
+    private $id;
+    private $username;
+    private $password;
+    private $role;
+
+    public function __construct(
+        string $id,
+        string $username,
+        string $password,
+        string $role
+    )
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getRoles(): array
+    {
+        return [$this->role];
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
