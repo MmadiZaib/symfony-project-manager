914ab708a8116a4991afef87aa12cce374417439
Mmadi Zaïb <zaib2010@gmail.com>
2021-07-26 22:13:49 +0200

    redirect to home page if it's already login

--- src/Controller/Auth/AuthController.php
+++ src/Controller/Auth/AuthController.php
@@ -18,6 +18,10 @@ class AuthController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
